ea2f984a5e327431eae5b6d09c7c4f9d9d589c32
BennyMarani <elmarbenny@gmail.com>
2022-06-02 18:20:15 +0700

    final project 01

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include ('partials/header.php'); ?>
+<?php include ('partials/nav.php'); ?>
+
+<?php 
+
+if(isset($_GET['page']) && $_GET['page'] == 'home'){
+// tampilkan halaman home.php
+include ('pages/home.php'); 
+
+} elseif(isset($_GET['page']) && $_GET['page'] == 'profile'){
+// tampilkan halaman profile.php
+include ('pages/profile.php'); 
+
+} elseif(isset($_GET['page']) && $_GET['page'] == 'gallery'){
+// tampilkan halaman gallery.php
+include ('pages/gallery.php'); 
+
+
+} elseif(isset($_GET['page']) && $_GET['page'] == 'contact'){
+// tampilkan halaman contact.php
+include ('pages/contact.php');
+
+} else{
+// tampilkan halaman error 404.php
+
+}
+
+
+
+
+
+
+ 
+
+
+?>
+
+
+<?php include ('partials/footer.php'); ?>
---
+++ pages/contact.php
@@ -0,0 +1,8 @@
+<!-- CONTENT -->
+<div class="row border p-3">
+    <div class="col-12">
+        <h1>contact</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur magnam animi, id accusamus accusantium rem qui, cumque repellendus delectus exercitationem hic maiores blanditiis cupiditate? Tempora corrupti nesciunt itaque recusandae necessitatibus!</p>
+    </div>
+</div>
+<!-- END CONTENT -->
